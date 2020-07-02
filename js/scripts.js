f2c6abc93da597fba5e98fdf3656a6de0b3dee6a
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-07-02 10:24:04 -0700

    hide deck of cards when button is toggled
    Co-authored-by: Vanessa Guerrero <vamarilug@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function() {
   $("#deck").submit(function(event) {
+
     event.preventDefault();
     const suits = [' hearts', ' diamonds', ' spades', ' clubs'];
     const cards = ["jack", "queen", "king", "ace", 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -10,10 +11,14 @@ $(document).ready(function() {
     suits.forEach(function(suit) {
       cards.forEach(function(card) {
         deck.push("<li>" + card + ' of ' + suit + "</li>");
+        
   });
 });
 
 // Business Logic
-  $("#wholedeck").append(deck);
+  $("#output").append(deck);
+   // $('#hide').click(function() {
+    $('#output').toggle(); {
+  }
   });
 });
